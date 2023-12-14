d1505cf7166e92c7824ea75b7d36263bf1f69594
mouhssine007 <mouhssinemajdi7@gmail.com>
2023-12-14 15:55:16 +0100

    test

---
+++ 02;leanr_python/exo.py
@@ -0,0 +1,8 @@
+firstName = input(" Please enter your first Name")
+
+LatName = input("Please enter your Last Name")
+resulte = firstName + LatName 
+
+email = firstName+"@gmail.com"
+print(email)
+print(resulte)
